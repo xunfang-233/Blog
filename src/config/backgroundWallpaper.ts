5271bc68be0933053964ae7110cbac3e3b4b0bb2
xunfang233 <1785803241@qq.com>
2026-08-01 22:19:40 +0800

    更新首页副标题

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -79,7 +79,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
-			subtitle: ["我或曾梦见,与你亲密无间;", "醒来后发现,你我形同陌路。"],
+			subtitle: ["我或曾梦见，与你亲密无间;", "醒来后发现，你我形同陌路。"],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
 			typewriter: {
