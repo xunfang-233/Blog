84fb893fe8541210676fe5b553e6dfe4e5bf5460
xunfang233 <1785803241@qq.com>
2026-08-02 09:49:39 +0800

    更新导航栏链接菜单

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -90,27 +90,15 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/xunfang-233/Blog",
 				external: true,
 				icon: "fa7-brands:github",
 			},
 			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
+				name: "B站",
+				url: "https://space.bilibili.com/219962071",
 				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
-			},
-			{
-				name: "Firefly文档",
-				url: "https://docs-firefly.cuteleaf.cn",
-				external: true,
-				icon: "material-symbols:docs",
+				icon: "simple-icons:bilibili",
 			},
 		],
 	});
