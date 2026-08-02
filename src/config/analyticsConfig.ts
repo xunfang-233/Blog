8ef0aed629b71a5762fc6886d3ba17d32fa56478
xunfang233 <1785803241@qq.com>
2026-08-02 11:25:10 +0800

    feat: 接入 51LA 网站统计

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -34,7 +34,7 @@ export const analyticsConfig: AnalyticsConfig = {
 	// 51la 统计配置
 	la51Analytics: {
 		// 51la 统计 ID
-		Id: "",
+		Id: "3QkjTipCZWPHWlKT",
 		// 自定义 SDK JS 地址，防止 DNS 污染，留空使用默认地址
 		sdkUrl: "",
 		// 多个统计 ID 的数据分离标识，留空则使用 Id
